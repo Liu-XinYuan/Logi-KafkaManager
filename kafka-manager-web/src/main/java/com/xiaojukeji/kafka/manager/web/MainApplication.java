00f9e47e83915e7d2558c4602253a8febf6f3068
Liu-XinYuan <879928098@qq.com>
2021-06-18 15:07:06 +0800

    测试环境  配置

--- kafka-manager-web/src/main/java/com/xiaojukeji/kafka/manager/web/MainApplication.java
+++ kafka-manager-web/src/main/java/com/xiaojukeji/kafka/manager/web/MainApplication.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -15,6 +16,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  */
 @EnableAsync
 @EnableScheduling
+@EnableFeignClients
 @ServletComponentScan
 @SpringBootApplication(scanBasePackages = {"com.xiaojukeji.kafka.manager"})
 public class MainApplication {
